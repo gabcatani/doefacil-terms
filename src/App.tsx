cbd50055a8382349b2e0381a28ca41b8e3a5e6db
Gabriel Catani <contatogcatani@gmail.com>
2023-11-14 18:12:56 -0300

    v1.0

---
+++ src/App.tsx
@@ -0,0 +1,103 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Termos e Condições de Uso do Doefacil</h1>
+      </header>
+      <main className="App-content">
+        <section>
+          <h2>1. Aceitação dos Termos</h2>
+          <p>
+            Ao acessar e usar o Doefacil, você concorda em cumprir estes termos e condições em sua totalidade. Se você não concorda com estes termos, não deve utilizar o aplicativo.
+          </p>
+        </section>
+
+        <section>
+          <h2>2. Alterações nos Termos</h2>
+          <p>
+            Reservamo-nos o direito de modificar ou substituir estes termos a qualquer momento. É sua responsabilidade revisar os termos periodicamente. Seu uso contínuo do aplicativo após quaisquer alterações significa que você aceita e concorda com as modificações.
+          </p>
+        </section>
+
+        <section>
+          <h2>3. Privacidade</h2>
+          <p>
+            Sua privacidade é importante para nós. Nossa Política de Privacidade está incorporada a estes Termos e Condições e explica como coletamos e utilizamos suas informações.
+          </p>
+        </section>
+
+        <section>
+          <h2>4. Uso Permitido</h2>
+          <p>
+            Você concorda em usar o Doefacil apenas para fins legais e de maneira que não infrinja os direitos de, restrinja ou iniba o uso e o gozo do aplicativo por qualquer outra parte.
+          </p>
+        </section>
+
+        <section>
+          <h2>5. Propriedade Intelectual</h2>
+          <p>
+            O conteúdo, design, código-fonte e outros materiais de propriedade intelectual contidos no Doefacil são de propriedade da Doefacil ou de nossos licenciadores e estão protegidos por leis de direitos autorais e marcas registradas.
+          </p>
+        </section>
+
+        <section>
+          <h2>6. Limitações de Uso</h2>
+          <p>
+            Você não deve:
+            <ul>
+              <li>Copiar, modificar ou distribuir o conteúdo do aplicativo sem nossa permissão prévia por escrito.</li>
+              <li>Utilizar o aplicativo para realizar qualquer atividade fraudulenta ou ilegal.</li>
+            </ul>
+          </p>
+        </section>
+
+        <section>
+          <h2>7. Contribuições dos Usuários</h2>
+          <p>
+            Ao enviar qualquer conteúdo para o nosso aplicativo, você concorda em conceder-nos um direito não exclusivo, perpétuo, irrevogável, livre de royalties para usar, reproduzir, modificar, publicar, traduzir e distribuir esse conteúdo em qualquer meio.
+          </p>
+        </section>
+
+        <section>
+          <h2>8. Isenção de Garantias</h2>
+          <p>
+            O Doefacil é fornecido "como está" e "como disponível", sem garantias de qualquer tipo.
+          </p>
+        </section>
+
+        <section>
+          <h2>9. Limitação de Responsabilidade</h2>
+          <p>
+            Não seremos responsáveis por quaisquer danos indiretos, incidentais, especiais ou consequenciais decorrentes do uso ou incapacidade de usar o aplicativo.
+          </p>
+        </section>
+
+        <section>
+          <h2>10. Lei Aplicável</h2>
+          <p>
+            Estes termos serão regidos e interpretados de acordo com as leis do país de jurisdição do aplicativo, sem considerar conflitos de disposições legais.
+          </p>
+        </section>
+
+        <section>
+          <h2>11. Contato</h2>
+          <p>
+            Para quaisquer questões ou esclarecimentos sobre estes termos, por favor, entre em contato conosco através do [inserir método de contato].
+          </p>
+        </section>
+
+        <section>
+          <p>Última atualização: [inserir data]</p>
+        </section>
+        
+      </main>
+      <footer className="App-footer">
+        <p>&copy; 2023 Doefacil. Todos os direitos reservados.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
